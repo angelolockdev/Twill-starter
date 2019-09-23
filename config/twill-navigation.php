747459b836c992bcab299254e7d118a4a6f8eca3
Angelo <a.locktohang@fulldigits.com>
2019-09-23 14:15:59 +0300

    Starter twill opérationnelle

---
+++ config/twill-navigation.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Menus' => [
+        'title' => 'Menus',
+        'module' => false,
+    ],
+    'Menus2' => [
+        'title' => 'Menus 2',
+        'module' => false,
+    ],
+];
