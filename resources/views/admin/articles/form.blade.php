0ec7101e68ec804ee573702c4ad6d4182e6804a4
Angelo <a.locktohang@fulldigits.com>
2019-09-23 16:23:46 +0300

    [Error] The views styles not working

--- resources/views/admin/articles/form.blade.php
+++ resources/views/admin/articles/form.blade.php
@@ -12,42 +12,9 @@
         'label' => 'Hero_image',
     ])
     
-    @formField('date_picker', [
-    'name' => 'event_date',
-    'label' => 'Event date',
-    'minDate' => '2019-09-10 12:00',
-    'maxDate' => '2019-12-10 12:00'
-    ])
-
-    @formField('select', [
-        'name' => 'discipline',
-        'label' => 'Discipline',
-        'unpack' => true,
-        'options' => [
-            [
-                'value' => 'arts',
-                'label' => 'Arts & Culture'
-            ],
-            [
-                'value' => 'finance',
-                'label' => 'Banking & Finance'
-            ],
-            [
-                'value' => 'civic',
-                'label' => 'Civic & Public'
-            ],
-            [
-                'value' => 'design',
-                'label' => 'Design & Architecture'
-            ],
-            [
-                'value' => 'education',
-                'label' => 'Education'
-            ],
-            [
-                'value' => 'entertainment',
-                'label' => 'Entertainment'
-            ],
+    @formField('block_editor', [
+        'blocks' => [
+            'gallery', 'image_with_text', 'quote', 'paragraph'
         ]
-    ])
+    ]);
 @stop
